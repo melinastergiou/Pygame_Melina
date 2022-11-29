47e082265d77e90c6e7a56c90e28cd9640285c4c
melinastergiou <melina.belli.stergiou@gmail.com>
2022-11-29 00:31:15 -0300

    iniciando assets

--- ferramentas.py
+++ ferramentas.py
@@ -2,6 +2,18 @@ import pygame
 from jogo import *
 # assets
 
+# ----- Inicia assets
+largura_comp = 51
+altura_comp = 51
+altura_carro = 2 * altura_comp
+velocidade = 6
+posições = [36,156,278,398]
+lista_posv = [posições[0], posições[1]]
+lista_posa = [posições[2], posições[3]]
+pontuação = 0
+font = pygame.font.SysFont(None, 48)
+background = pygame.image.load('recursos/fundo.png').convert()
+
 # --- imagens ---
 
 
