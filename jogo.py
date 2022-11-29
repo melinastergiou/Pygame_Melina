61b6941fd65e7c78c052e020e9a2ef22a1d5f540
melinastergiou <melina.belli.stergiou@gmail.com>
2022-11-29 12:48:52 -0300

    adicionando som e arrumando código

--- jogo.py
+++ jogo.py
@@ -1,22 +1,190 @@
 
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
 import pygame 
 import random
-from ferramentas import *
 
 pygame.init()
 
 # ----- Gera tela principal
 largura_da_tela = 484
-altura_da_tela = 968
+altura_da_tela = 650
 window = pygame.display.set_mode((largura_da_tela, altura_da_tela))
-pygame.display.set_caption('jogo')
+pygame.display.set_caption('Carrinhos UHU!')
+
+background_image= pygame.image.load('D:\Insper\DeSoft\Pygame_Melina\imagens/cargame.png').convert_alpha()
+sound_menu = pygame.mixer.Sound('D:\Insper\DeSoft\Pygame_Melina\sons/menu.wav')
 
 
+# ----- cria funções
+def tela_menu_inicial(tela):
+    
+    # FUNDO 
+    tela.blit(background_image, (0,0))
+    
+    return None
+
+def cria_obstaculos_seguintes():
+    obstaculos_vermelho = [1,2]
+    obstaculos_azul = [1,2]
+
+    distv = random.randrange(2*altura_comp)
+    dista = random.randrange(2*altura_comp)
+
+    pverm = random.choice(obstaculos_vermelho)
+    pazul = random.choice(obstaculos_azul)
+
+    if pverm == 1:
+        posv = posições[0]
+    else:
+        posv = posições[1]
+
+    if pazul == 1:
+        posa = posições[2]
+    else:
+        posa = posições[3]
+
+    overm = random.choice(obstaculos_vermelho)
+    oazul = random.choice(obstaculos_azul)
+
+    if overm == 1:
+        obv = Quadrado(quadrado_v_img, posv, distv)
+        todosquadrados.add(obv)
+    else:
+        obv = Circulo(circulo_v_img, posv, distv)
+        todoscirculos.add(obv)
+
+    if oazul == 1:
+        oba = Quadrado(quadrado_a_img, posa, dista)
+        todosquadrados.add(oba)
+    else:
+        oba = Circulo(circulo_a_img, posa, dista)
+        todoscirculos.add(oba)
+
+    sprites.add(obv,oba)
+
+    return True   
+
+# ----- Inicia assets
+largura_comp = 51
+altura_comp = 51
+altura_carro = 2 * altura_comp
+velocidade = 6
+posições = [36,156,278,398]
+lista_posv = [posições[0], posições[1]]
+lista_posa = [posições[2], posições[3]]
+pontuação = 0
+font = pygame.font.SysFont(None, 48)
+background = pygame.image.load('D:\Insper\DeSoft\Pygame_Melina\imagens/fundo.png').convert()
+
+quadrado_v_img = pygame.image.load('D:\Insper\DeSoft\Pygame_Melina\imagens/quadrado_v.png').convert_alpha()
+circulo_v_img = pygame.image.load('D:\Insper\DeSoft\Pygame_Melina\imagens/circulo_v.png').convert_alpha()
+carro_v_img = pygame.image.load('D:\Insper\DeSoft\Pygame_Melina\imagens/carro_v.png')
+
+quadrado_a_img = pygame.image.load('D:\Insper\DeSoft\Pygame_Melina\imagens/quadrado_a.png').convert_alpha()
+circulo_a_img = pygame.image.load('D:\Insper\DeSoft\Pygame_Melina\imagens/circulo_a.png').convert_alpha()
+carro_a_img = pygame.image.load('D:\Insper\DeSoft\Pygame_Melina\imagens/carro_a.png')
+
+quadrado_v_img = pygame.transform.scale(quadrado_v_img, (largura_comp, altura_comp))
+circulo_v_img = pygame.transform.scale(circulo_v_img, (largura_comp, altura_comp))
+carro_v_img = pygame.transform.scale(carro_v_img, (largura_comp, altura_carro))
+
+quadrado_a_img = pygame.transform.scale(quadrado_a_img, (largura_comp, altura_comp))
+circulo_a_img = pygame.transform.scale(circulo_a_img, (largura_comp, altura_comp))
+carro_a_img = pygame.transform.scale(carro_a_img, (largura_comp, altura_carro))
+
+borda_img = pygame.image.load('D:\Insper\DeSoft\Pygame_Melina\imagens/Borda.PNG').convert()
+borda_img = pygame.transform.scale(borda_img, (largura_da_tela, 1))
+
+# ----- Cria classes
+class Carro(pygame.sprite.Sprite):
+    def __init__(self, img, cor):
+        pygame.sprite.Sprite.__init__(self)
+
+        #self.velocidade = aumento_da_velocidade
+        self.image = img
+        self.rect = self.image.get_rect()
+        if cor == 'v':
+            self.rect.x = posições[0]
+            self.pos = [posições[0], posições[1]]
+
+        else:
+            self.rect.x = posições[2]
+            self.pos = [posições[2], posições[3]]
+
+        self.rect.y = altura_da_tela - altura_carro - altura_comp - 9
+        self.speedx = 0
+        self.speedy = 0
+        self.cor = cor
+        self.indice = 0
+
+    def update (self):
+        self.rect.x += self.speedx
+
+        if self.cor == 'v':
+            if self.speedx != 0 and self.rect.x in lista_posv:
+                self.speedx = 0
+        else:
+            if self.speedx != 0 and self.rect.x in lista_posa:
+                self.speedx = 0
+
+class Quadrado(pygame.sprite.Sprite):
+    def __init__(self, img, pos, dist):
+        pygame.sprite.Sprite.__init__(self)
+
+        #self.velocidade = aumento_da_velocidade
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.c = 'n'
+        self.rect.x = pos
+        self.rect.y = -dist
+        self.speedx = 0
+        self.speedy = velocidade
+
+    def update (self):
+        if self.rect.y >= altura_da_tela/3 and self.c == 'n' and self.rect.x <= 242:
+            cria_obstaculos_seguintes()
+            self.c = 's'
+        self.rect.y += self.speedy
+
+class Circulo(pygame.sprite.Sprite):
+    def __init__(self, img, pos, dist):
+        pygame.sprite.Sprite.__init__(self)
+
+        #self.velocidade = aumento_da_velocidade
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.c = 'n'
+        self.rect.x = pos
+        self.rect.y = -dist 
+        self.speedx = 0
+        self.speedy = velocidade
+        self.indice = 0
+
+    def update (self):
+        if self.rect.y >= altura_da_tela/3 and self.c == 'n' and self.rect.x <= 242:
+            cria_obstaculos_seguintes()
+            self.c = 's'
+        self.rect.y += self.speedy
+
+class Borda(pygame.sprite.Sprite):
+    def __init__(self, img, borda):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        if borda == 1:
+            self.rect.y = altura_da_tela
+        else: 
+            self.rect.y = altura_da_tela + altura_comp
+
+
+# ----- Inicia estruturas de dados
 game = True
 
 clock = pygame.time.Clock()
 FPS = 60
-
 
 # ----- criando obstaculos
 
@@ -43,9 +211,11 @@ sprites.add(carro_a)
 sprites.add(cvinicial)
 sprites.add(cainicial)
 
+# tela menu inicial
 
 # ===== Loop principal =====
 while game:
+    sound_menu.play()
     clock.tick(FPS)
 
     # ----- Trata eventos
@@ -54,12 +224,10 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
-            # serão criados (carro a carro v)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT and carro_a.indice != 0:
                 carro_a.speedx = -12
-                carro_a.indice=0
-            
+                carro_a.indice = 0
 
             if event.key == pygame.K_RIGHT and carro_a.indice != 1:
                 carro_a.speedx = 12
@@ -75,7 +243,9 @@ while game:
 
     sprites.update()
 
+# FALTA COLOCAR A PARTIR DAQUI + SOM + POSSÍVEL ANIMAÇÃO
     
+
     # ----- Verifica Colisões
     hitsquadradosv = pygame.sprite.spritecollide(carro_v, todosquadrados, True)
     hitsquadradosa = pygame.sprite.spritecollide(carro_a, todosquadrados, True)
@@ -106,4 +276,4 @@ while game:
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
-pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+pygame.quit()  # Função do PyGame que finaliza os D:\Insper\DeSoft\Pygame_Melina\imagens utilizados
